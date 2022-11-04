0c96dbe58206f7c9cfd8366ca279bf669c0a5299
TLRKiliann <philogenie@protonmail.com>
2022-11-04 15:32:06 +0100

    add new routes & new files

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-
 import { NavBar } from './components/NavBar'
-
 import { Login } from './Pages/Login'
 import { MyCarousel } from './Pages/MyCarousel'
 import { Animation } from './Pages/Animation'
 import { About } from './Pages/About' 
-
+import { Profile } from './Pages/Profile' 
+import { Settings } from './Pages/Settings' 
+import { SignOut } from './Pages/SignOut' 
 import './App.scss'
 
 const App:React.FC = () => {
@@ -18,6 +18,9 @@ const App:React.FC = () => {
         <Route path='/carousel' element={<MyCarousel />} />
         <Route path='/animation' element={<Animation />} />
         <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/settings' element={<Settings />} />
+        <Route path='/signout' element={<SignOut />} />
       </Routes>
     </div>
   )
--- vite-project/src/components/NavBar.tsx
+++ vite-project/src/components/NavBar.tsx
@@ -12,47 +12,40 @@ export const NavBar:React.FC = () => {
     eraseAll()
   };*/
   return(
-    <nav className="bg-gray-800">
+    <nav className="bg-gray-800" id="mytweeks--navtag">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
+        <div className="relative flex h-20 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
 
-            <button type="button" className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-              <span className="sr-only">Open main menu</span>
-
-              <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-              </svg>
-
-              <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </button>
           </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center">
-              <img className="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
-              <img className="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
-            </div>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
+          <div className="flex flex-1 items-center justify-around sm:items-stretch sm:justify-start">
 
-                <li className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">
+            <div className="div--img">
+              <img
+                className="img--tag"
+                src={imageLogo} alt="logo_koala"
+              />
+            </div>
+
+            <div className="hidden sm:ml-10 sm:block">
+              <div className="flex space-x-8">
+
+                <li className="bg-gray-900 text-white px-3 py-2 rounded-md text-lg font-medium" aria-current="page">
                   <Link className="tag--link" to='/' >
-                    Login
+                    Dashboard
                   </Link>
                 </li>
-                <li className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                <li className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium">
                   <Link className="tag--link" to='/carousel' >
                     Carousel
                   </Link>
                 </li>
-                <li className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                <li className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium">
                   <Link className="tag--link" to='/animation' >
                     Animation
                   </Link>
                 </li>
-                <li className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                <li className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium">
                   <Link className="tag--link" to='/about' >
                     About
                   </Link>
@@ -75,15 +68,27 @@ export const NavBar:React.FC = () => {
                 <button type="button" className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                   <span className="sr-only">Open user menu</span>
                   <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                
                 </button>
               </div>
 
               <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
 
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-0">Your Profile</a>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-1">Settings</a>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-2">Sign out</a>
-
+                <li className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-0">
+                  <Link className="tag--link" to='/profile' >
+                    Your Profile
+                  </Link>
+                </li>
+                <li className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-1">
+                  <Link className="tag--link" to='/settings' >
+                    Settings
+                  </Link>
+                </li>
+                <li className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-2">
+                  <Link className="tag--link" to='/signout' >
+                    Sign out
+                  </Link>
+                </li>
                 <li className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-0">
                   <Link className="tag--link" to='/' >
                     Login
@@ -113,7 +118,6 @@ export const NavBar:React.FC = () => {
 
       <div className="sm:hidden" id="mobile-menu">
         <div className="space-y-1 px-2 pt-2 pb-3">
-
 
           <li className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">
             <Link className="tag--link" to='/' >
